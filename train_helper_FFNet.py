61eaac0a9b6857be633824214dcec4976dd2f268
ZufarSyaafie <55452131+ZufarSyaafie@users.noreply.github.com>
2025-09-21 23:36:39 +0700

    "fix; error syntax"

--- train_helper_FFNet.py
+++ train_helper_FFNet.py
@@ -141,7 +141,7 @@ class Trainer(object):
                 shuffle=(True if x == "train" else False),
                 num_workers=args.num_workers * self.device_count,
                 pin_memory=(True if x == "train" else False),
-                drop_last=(True if x == "train" else False)
+                drop_last=(True if x == "train" else False),
             )
             for x in ["train", "val"]
         }
@@ -527,5 +527,6 @@ if __name__ == "__main__":
     x = torch.ones(1, 3, 768, 1152)
     y = tensor_spilt(x)
     print(y.size())
+
 
 
